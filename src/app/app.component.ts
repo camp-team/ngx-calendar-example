87cb3d0f6ba5b2cb66a45aebc53ade6d661ebc28
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2020-04-26 18:31:11 +0900

    diff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { EventData } from 'ngx-event-calendar/lib/interface/event-data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'calendar-test';
+  dataArray: EventData[] = [
+    {
+      id: 1,
+      title: 'aaa',
+      startDate: new Date(),
+      endDate: new Date(),
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { NgxEventCalendarModule } from "ngx-event-calendar";
 
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    NgxEventCalendarModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
